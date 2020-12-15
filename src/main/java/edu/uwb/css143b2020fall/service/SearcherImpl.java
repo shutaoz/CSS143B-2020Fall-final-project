1fee553b610198770c61f208c57a9fc3feba8639
shutaozhang <shutao76@uw.edu>
2020-12-15 14:22:35 -0800

    Searcher and Indexer pass their tests and the integration test. 3 EC tests included

--- src/main/java/edu/uwb/css143b2020fall/service/SearcherImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/SearcherImpl.java
@@ -43,31 +43,7 @@ public class SearcherImpl implements Searcher {
 
         }
 
-        /*
-        if(temp.length > 1){
 
-        //a generated list of indices
-
-            List<List<Integer>> placeHolder = new LinkedList<>();
-
-            //find indices of all the words
-            for(int i = 0; i < temp.length; ++i){
-                placeHolder.add(findIndices(temp[i], index));
-            }
-
-            result = findSharedDocs(placeHolder);
-
-            result = inRightOrder(temp, result, index);
-
-            return result;
-
-
-
-
-
-        }
-
-         */
 
         if(temp.length > 1){
 
@@ -93,97 +69,6 @@ public class SearcherImpl implements Searcher {
         return flag;
     }
 
-    /*
-    //check if every word is inside a specific document
-    public boolean isInsideDoc(String[] uWord, List<Integer> doc){
-        boolean flag = true;
-        for(int i = 0; i < uWord.length; ++i){
-            if(!doc.contains(uWord[i]))
-                flag = false;
-        }
-
-
-        return flag;
-
-    }
-
-     */
-    /*
-
-
-    public List<Integer> findIndices(String uWord, Map<String, List<List<Integer>>> index){
-        List<Integer> indices = new LinkedList<>();
-        //?
-        for(int i = 0; i < index.get(uWord).size(); ++i){
-            if(!index.get(uWord).get(i).isEmpty())
-                indices.add(i);
-
-        }
-        return indices;
-    }
-
-    public List<Integer> findSharedDocs(List<List<Integer>> indices){
-        List<Integer> resList = new LinkedList<>();
-        Integer[] resArray = new Integer[indices.size()];
-
-        //compare the first two index lists
-        //a one step process
-        resArray = findCommon(indices.get(0), indices.get(1));
-
-        //conversion into a list to be used in the findCommon method
-        for(int i = 0; i < resArray.length; ++i)
-            resList.add(resArray[i]);
-
-        for(int i = 2; i < indices.size() - 1; ++i){
-            resArray = findCommon(resList, indices.get(i));
-            //?
-            resList.removeAll(resList);
-
-            for(int j = 0; j < resArray.length; ++j)
-                resList.add(resArray[j]);
-
-
-        }
-        return resList;
-
-
-    }
-
-    public Integer[] findCommon(List<Integer> listA, List<Integer> listB){
-        Set<Integer> setA = new HashSet<>();
-        Set<Integer> setB = new HashSet<>();
-
-
-        for(int i = 0; i < listA.size(); ++i)
-            setA.add(listA.get(i));
-
-
-        for(int i = 0; i < listB.size(); ++i)
-            setB.add(listB.get(i));
-
-        setA.retainAll(setB);
-
-        Integer[] result = new Integer[setA.toArray().length];
-        for(int i = 0; i < setA.toArray().length; ++i){
-            result[i] = (Integer) setA.toArray()[i];
-        }
-        return result;
-
-    }
-
-    //words passed in are the searched keywords
-    //uWord is created to facilitate the index finding process
-    //another refinement step to find docs that have the words in the right order
-    public List<Integer> inRightOrder(String[] words, List<Integer> commonDoc, Map<String, List<List<Integer>>> index){
-
-        for(int i = 0; i < (words.length - 1); ++i){
-            if(-1 != (index.get(words[i]).get(commonDoc.get(i)).indexOf(words[i]) - index.get(words[i + 1]).get(commonDoc.get(i + 1)).indexOf(words[i + 1])))
-                commonDoc.remove((Integer)i);
-        }
-        return commonDoc;
-    }
-
-     */
 
 
     public List<Integer> commonDocs(String[] words,Map<String, List<List<Integer>>> index){
@@ -222,6 +107,8 @@ public class SearcherImpl implements Searcher {
                     while(index.get(words[j + 1]).get(docs.get(i)).get(temp) < index.get(words[j]).get(docs.get(i)).get(0)) {
                         if(temp + 1 < index.get(words[j + 1]).get(docs.get(i)).size())
                             ++temp;
+                        else
+                            break;
                     }
                     if ((index.get(words[j]).get(docs.get(i)).get(x) - index.get(words[j + 1]).get(docs.get(i)).get(temp)) != -1 )
                         flag = false;
